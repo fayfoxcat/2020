f81700500ac728b34def633e1577cff83aefeb92
fox <fox@gamil.com>
2021-08-23 18:38:20 +0800

    ...

--- consumer/src/main/java/com/fox/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/fox/consumer/controller/ConsumerController.java
@@ -1,18 +1,14 @@
 package com.fox.consumer.controller;
 
 import com.fox.consumer.service.ProviderClient;
-import com.mongodb.client.MongoCollection;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api")
