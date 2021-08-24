11d3e79e937aebb04b916b3ed004689bbec0e167
fox <fox@gamil.com>
2021-08-24 17:35:27 +0800

    ...

--- consumer/src/main/java/com/fox/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/fox/consumer/controller/ConsumerController.java
@@ -1,14 +1,15 @@
 package com.fox.consumer.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.fox.consumer.service.ProviderClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("api")
@@ -25,6 +26,22 @@ public class ConsumerController {
     @GetMapping("/flow")
     public void flow() {
 
+    }
+
+    @GetMapping("/hi")
+    @SentinelResource(value = "qps")
+    public Map<String, Object> hi(@RequestParam(value = "name", defaultValue = "forezp", required = false) String name) {
+        Map<String, Object> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add("中国");
+        list.add("江苏");
+        list.add("南京");
+        list.add("玄武");
+        map.put("id",10086);
+        map.put("name","徐凤年");
+        map.put("age",18);
+        map.put("address",list);
+        return map;
     }
 
     public static void main(String[] args) {
